533d7b93e0fa34bff09af030ea96543e3a375fa9
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-17 12:10:42 +0200

    create and finish component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Logo & Favicon created by{" "}
+        <a href="https://freepik.com" target="_blank" rel="noreferrer">
+          Freepik
+        </a>
+      </p>
+    </footer>
+  );
+}
