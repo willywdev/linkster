e6fc47a22751b386fd571afbaeab6d6334956ef6
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-17 11:59:44 +0200

    create basic component Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
+import "../../../public/github.svg";
 import "./Header.css";
 
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="header__headline">Hello World!</h1>
+      <h1 className="header__headline">Linkster</h1>
+      <div className="header__github-wrapper">
+        <a
+          href="https://github.com/willywdev"
+          className="header__github-link"
+          target="_blank"
+          rel="noreferrer">
+          <img src="./github.svg" className="header__github-logo"></img>
+          <p>Github</p>
+        </a>
+      </div>
     </header>
   );
 }
