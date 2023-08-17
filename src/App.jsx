c3e76afa6f9dcadcba22e70e7c1c9f5c2a24ce56
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-17 12:28:36 +0200

    create Component MainContent, delete asset folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-
+import MainContent from "./components/MainContent/MainContent";
 function App() {
   return (
     <>
       <Header />
+      <MainContent />
       <Footer />
     </>
   );
---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,16 @@
+import "./MainContent.css";
+
+export default function MainContent() {
+  return (
+    <section className="main-content__section">
+      <article className="main-content__card">
+        <img
+          src="./assets/2993773_git_social media_icon.svg"
+          className="main-content__card-logo"></img>
+        <a href="" target="_blank" className="main-content__card-link">
+          Hello World
+        </a>
+      </article>
+    </section>
+  );
+}
