d1982897b58f8b39ebf7234fb1c8179c1be90213
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-17 13:08:14 +0200

    fix some styling

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -3,7 +3,7 @@ import git from "./assets/2993773_git_social media_icon.svg";
 
 export default function MainContent() {
   return (
-    <main className="main-content__main">
+    <>
       <section className="main-content__section">
         <a href="" target="_blank" className="main-content__card-link">
           <article className="main-content__card">
@@ -12,6 +12,14 @@ export default function MainContent() {
           </article>
         </a>
       </section>
-    </main>
+      <section className="main-content__section">
+        <a href="" target="_blank" className="main-content__card-link">
+          <article className="main-content__card">
+            <img src={git} className="main-content__card-logo"></img>
+            <p>Git Warlock 🔮</p>
+          </article>
+        </a>
+      </section>
+    </>
   );
 }
