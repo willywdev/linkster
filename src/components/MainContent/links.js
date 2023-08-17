c7d52d2d77d49908b5209a8155fcc9687282f517
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-17 19:49:48 +0200

    add more content

--- src/components/MainContent/links.js
+++ src/components/MainContent/links.js
@@ -5,9 +5,29 @@ const links = [
     url: "https://github.com/willywdev/git-warlock",
   },
   {
-    imgSrc: "./2993773_git_social media_icon.svg",
-    text: "Test",
-    url: "",
+    imgSrc: "./icons8-morty.svg",
+    text: "Rick and Morty Character Log",
+    url: "https://willywdev.github.io/rick-and-morty-app/",
+  },
+  {
+    imgSrc: "./icons8-scissor-64.png",
+    text: "LinkTrim - Shorten your links",
+    url: "https://linktrim.willydev.de/",
+  },
+  {
+    imgSrc: "./icons8-cheat-engine.svg",
+    text: "Awesome Cheatsheets for Web Developers",
+    url: "https://willywdev.github.io/awesome-cheatsheets/",
+  },
+  {
+    imgSrc: "./icons8-vs-code.svg",
+    text: "VS Code Extensions: Web Dev Essentials + Awesome Themes",
+    url: "https://marketplace.visualstudio.com/items?itemName=willywdev.willys-webdev-extensions",
+  },
+  {
+    imgSrc: "./icons8-restaurant-96.png",
+    text: "TasteTrackr - Rate your own restaurants in your personal profile (Unfinished)",
+    url: "https://taste-trackr-d6c61.web.app/",
   },
 ];
 
