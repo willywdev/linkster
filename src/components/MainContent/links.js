b6e03fd3c492d31e113e3a2b7dd4b38d3c7344ae
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-17 13:39:43 +0200

    trying to fix img url path with github pages

--- src/components/MainContent/links.js
+++ src/components/MainContent/links.js
@@ -1,13 +1,13 @@
 const links = [
   {
     imgSrc:
-      "src/components/MainContent/assets/2993773_git_social media_icon.svg",
+      "/src/components/MainContent/assets/2993773_git_social media_icon.svg",
     text: "Git Warlock 🔮",
     url: "https://github.com/willywdev/git-warlock",
   },
   {
     imgSrc:
-      "src/components/MainContent/assets/2993773_git_social media_icon.svg",
+      "/src/components/MainContent/assets/2993773_git_social media_icon.svg",
     text: "Test",
     url: "",
   },
