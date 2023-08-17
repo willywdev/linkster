4fba11abdffd990d02cf811cbfc2c830fb50bb28
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-17 11:21:43 +0200

    create Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__headline">Hello World!</h1>
+    </header>
+  );
+}
