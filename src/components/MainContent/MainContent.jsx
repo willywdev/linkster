68a1c17299403a4e5e124aa39e40bb86637ab5af
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-24 12:49:39 +0200

    add functions to render content dynamically

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -3,12 +3,43 @@ import links from "./links";
 
 export default function MainContent() {
   return (
-    <>
-      <RenderVanilla />
-      {links.map((link, index) => (
-        <section key={index} className="main-content__section">
+    <div className="main-content">
+      <RenderLanguages language="vanilla" />
+      <RenderLanguages language="general" />
+    </div>
+  );
+}
+
+function RenderLanguages({ language }) {
+  const icons = [{ name: "vanilla", images: ["/icons8-morty.svg", "2", "3"] }];
+  return (
+    <section className="main-content__section">
+      <h1 className="main-content__card-render-language">
+        {language.toUpperCase()}{" "}
+        {icons
+          .filter((icon) => {
+            return icon.name === language;
+          })
+          .map((icon) => {
+            return icon.images.map((image, index) => {
+              return (
+                <img
+                  key={index}
+                  src={image}
+                  className="main-content__headline-image"
+                />
+              );
+            });
+          })}
+      </h1>
+      {links
+        .filter((link) => {
+          return link.language.includes(language);
+        })
+        .map((link, index) => (
           <a
             href={link.url}
+            key={index}
             target="_blank"
             className="main-content__card-link"
             rel="noreferrer">
@@ -21,14 +52,7 @@ export default function MainContent() {
               <p className="main-content__card-text">{link.text}</p>
             </article>
           </a>
-        </section>
-      ))}
-    </>
-  );
-}
-
-function RenderVanilla() {
-  return (
-    <h1 className="main-content__card-render-language">Vanilla HTML,CSS,JS</h1>
+        ))}
+    </section>
   );
 }
--- src/components/MainContent/links.js
+++ src/components/MainContent/links.js
@@ -3,31 +3,37 @@ const links = [
     imgSrc: "./2993773_git_social media_icon.svg",
     text: "Git Warlock 🔮",
     url: "https://github.com/willywdev/git-warlock",
+    language: "python",
   },
   {
     imgSrc: "./icons8-morty.svg",
     text: "Rick and Morty Character Log",
     url: "https://willywdev.github.io/rick-and-morty-app/",
+    language: "vanilla",
   },
   {
     imgSrc: "./icons8-scissor-64.png",
     text: "LinkTrim - Shorten your links",
     url: "https://linktrim.willydev.de/",
+    language: "vanilla",
   },
   {
     imgSrc: "./icons8-cheat-engine.svg",
     text: "Awesome Cheatsheets for Web Developers",
     url: "https://willywdev.github.io/awesome-cheatsheets/",
+    language: "vanilla",
   },
   {
     imgSrc: "./icons8-vs-code.svg",
     text: "VS Code Extensions: Web Dev Essentials + Awesome Themes",
     url: "https://marketplace.visualstudio.com/items?itemName=willywdev.willys-webdev-extensions",
+    language: "general",
   },
   {
     imgSrc: "./icons8-restaurant-96.png",
     text: "TasteTrackr - Rate your own restaurants in your personal profile (Unfinished)",
     url: "https://taste-trackr-d6c61.web.app/",
+    language: "vanilla, firebase",
   },
 ];
 
